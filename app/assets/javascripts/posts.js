99619073517acce89f524dc30dd559eaa6e28a41
febbraiod <donedesign1@gmail.com>
2016-06-16 13:15:14 -0400

    took out jQuery on blog index in favor of show links

--- app/assets/javascripts/posts.js
+++ app/assets/javascripts/posts.js
@@ -1,10 +1,10 @@
-$(function(){
-  $('.js-more').on('click', function(){
-    var slug = $(this).attr("data-id");
-    $.get("/blog/" + slug + ".json", function(resp){
-      $("#post_" + slug).text(resp.post.content);
-      $("#post_" + slug + "_images").removeClass('hidden');
-    });
-    $(this).hide();
-  });
-});
+// $(function(){
+//   $('.js-more').on('click', function(){
+//     var slug = $(this).attr("data-id");
+//     $.get("/blog/" + slug + ".json", function(resp){
+//       $("#post_" + slug).text(resp.post.content);
+//       $("#post_" + slug + "_images").removeClass('hidden');
+//     });
+//     $(this).hide();
+//   });
+// });
