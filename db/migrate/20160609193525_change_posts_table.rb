73f6f42249bb587c99d4a49b802e608f2491a519
febbraiod <donedesign1@gmail.com>
2016-06-17 11:58:42 -0400

    testing body width on heroku

--- db/migrate/20160609193525_change_posts_table.rb
+++ db/migrate/20160609193525_change_posts_table.rb
@@ -2,10 +2,5 @@ class ChangePostsTable < ActiveRecord::Migration
   def change
     remove_column :posts, :icon
     add_column :posts, :icon_id, :integer
-
-    add_column :posts, :icon_file_name
-    add_column :posts, :icon_content_type
-    add_column :posts, :icon_file_size
-    add_column :posts, :icon_updated_at
   end
 end
