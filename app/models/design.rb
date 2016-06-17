43addddc0337ef58e450c06e67aedef7d0e7e823
febbraiod <donedesign1@gmail.com>
2016-06-17 17:19:04 -0400

    remove placeholder thumb image with empty div for better heroku display

--- app/models/design.rb
+++ app/models/design.rb
@@ -2,22 +2,16 @@ class Design < ActiveRecord::Base
   has_one :gallery
 
   def gallery=(gallery_attributes)
-    binding.pry
     if self.gallery
-      binding.pry
       self.gallery.name = gallery_attributes[:name]
-      binding.pry
       self.gallery.save
     else
-      binding.pry
       self.build_gallery(name: gallery_attributes[:name])
-      binding.pry
       self.gallery.save
     end
   end
 
   def add_photos(photos)
-    binding.pry
     photos.sort_by {|p| p.original_filename}.each do |pic|
       self.gallery.photos << Photo.create(pic: pic)
     end
