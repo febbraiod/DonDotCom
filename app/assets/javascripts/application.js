24fb17d2431708cd9cbc1e83b369769fb3ec6af2
febbraiod <donedesign1@gmail.com>
2016-06-29 13:12:32 -0400

    front_end stuff

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -54,6 +54,11 @@ function place_blog_images(){
   $('.img_3').html($('.post_image_3'));
   $('.img_4').html($('.post_image_4'));
   $('.img_5').html($('.post_image_5'));
+  $('.img_6').html($('.post_image_6'));
+  $('.img_7').html($('.post_image_7'));
+  $('.img_8').html($('.post_image_8'));
+  $('.img_9').html($('.post_image_9'));
+  $('.img_10').html($('.post_image_10'));
 }
 
 
