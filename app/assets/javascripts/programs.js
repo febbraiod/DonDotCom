cc2f7cbbf2e5f594d58caf710df5be2b778052a1
febbraiod <donedesign1@gmail.com>
2016-10-14 16:31:45 -0400

    add hover effect on program header

--- app/assets/javascripts/programs.js
+++ app/assets/javascripts/programs.js
@@ -1,7 +1,20 @@
-
 function bind_program_thumbs(){
   $('.program_thumb').hover(function(){
     var image_url = this.src;
     var index = $(this).data('index');
     $('#view_' + index).children()[0].src = image_url = image_url;});
+}
+
+function hover_header(){
+  $('.fixer').hover(function(){
+    $('.program_wrapper_blocker').stop(true, true).fadeTo(400, 0);
+
+    $('#program_navbar').attr("id","navbar");
+
+    }, function(){
+    $('.program_wrapper_blocker').stop(true, true).fadeTo(300, 1);
+
+    $('#navbar').attr("id","program_navbar");
+
+    });
 }
